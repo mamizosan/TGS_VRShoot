a1da2dc786e8e6c79b7a4d492a82ece7f149f6f0
mamizosan <ymjftokinotabibito@gmail.com>
2019-07-12 05:20:03 +0900

    reuproad

---
+++ VRzemi_vr2/Assets/Scripts/Result/ResultManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResultManager : ScenesManager {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            NextScene();
+        }
+		
+	}
+
+    protected override void NextScene()
+    {
+        _scene_changer.SceneChange(StringConstantRegistry.SCENE_NAME.START);
+    }
+
+
+}
